f447dcb063d401f5948dc3e32567d49532745fa5
dazzlevincent <dazzvincent@gmail.com>
2024-06-08 00:23:50 +0530

    Completed

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddProduct = () => {
+    return (
+        <div>
+            <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-control">Product ID</label>
+                                <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-control">Product Name</label>
+                            <input type="text" className="form-control" />
+
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-control">Product Description</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-control">Price</label>
+                            <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-control">Brand</label>
+                            <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">SUBMIT</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default AddProduct
---
+++ src/components/DeleteProduct.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const DeleteProduct = () => {
+  return (
+    <div>
+        
+        <NavBar/>
+        <div className="container">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-control">Delete Product</label>
+                                <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">DELETE</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        </div>
+  )
+}
+
+export default DeleteProduct
